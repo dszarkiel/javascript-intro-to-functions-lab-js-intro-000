e9e9ddd9297f58240b647e90770dfb3bf194e931
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-08-14 18:57:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,15 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+var uppercase = "HELLO"
+var lowercase = "hello"
+var mixedCase = "I love you, Grandma."
+
 function sayHiToGrandma(string) {
-  if(string.toLowerCase() === lowercase) {
+  if(string === lowercase) {
     return "I can't hear you!"
-  } else if(string.toUpperCase() === uppercase) {
-    return "YES INDEED!"
+  } else if(string === uppercase) {
+        return "YES INDEED!"
   }  else if(string === "I love you, Grandma.") {
         return "I love you, too."
   }
